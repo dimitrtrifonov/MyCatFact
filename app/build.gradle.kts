afdd7b1afaa63058b117239b0abd9f673532c30f
Dimitr Trifonov <dimitr.trifonov@gmail.com>
2025-02-16 13:30:37 +0100

    add UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,8 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
--- app/src/main/java/clarity/software/dimtrif/mycatfact/MainActivity.kt
+++ app/src/main/java/clarity/software/dimtrif/mycatfact/MainActivity.kt
@@ -4,26 +4,61 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import clarity.software.dimtrif.mycatfact.ui.theme.MyCatFactTheme
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyCatFactTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            CatBreedsScreen()
+        }
+    }
+}
+
+
+@Composable
+fun CatBreedsScreen(catViewModel: CatBreedsViewModel = viewModel()) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text("Cat Breeds", style = MaterialTheme.typography.labelMedium)
+
+        when {
+            catViewModel.isLoading.collectAsState().value -> CircularProgressIndicator(
+                modifier = Modifier.padding(
+                    16.dp
+                )
+            )
+
+            catViewModel.errorMessage.collectAsState().value != null -> Text(
+                text = catViewModel.errorMessage.collectAsState().value!!,
+                color = MaterialTheme.colorScheme.error
+            )
+
+            else -> {
+                LazyColumn {
+                    items(catViewModel.catBreeds.value) { breed ->
+                        CatBreedItem(breed)
+                    }
                 }
             }
         }
@@ -31,17 +66,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyCatFactTheme {
-        Greeting("Android")
+fun CatBreedItem(catBreed: CatBreed) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = catBreed.breed, style = MaterialTheme.typography.labelMedium)
+            Text(text = "Origin: ${catBreed.origin}")
+            Text(text = "Country: ${catBreed.country}")
+            Text(text = "Coat: ${catBreed.coat}")
+            Text(text = "Pattern: ${catBreed.pattern}")
+        }
     }
 }
